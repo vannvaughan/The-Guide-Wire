0dbc77b3760007aee300fb8c51c6728a72225b2c
vannvaughan <vann.vaughan@yahoo.com>
2017-07-14 16:28:20 -0500

    heroku added

--- index.js
+++ index.js
@@ -3,23 +3,23 @@ const express = require('express')
     , session = require('express-session')
     , bodyParser = require('body-parser')
     , massive = require('massive')
-    , port = 3000
+    // , port = 3000
     , app = express()
     , masterRoutes = require('./masterRoutes.js')
-    , config = require('./server/config.js')
+    // , config = require('./server/config.js')
 
 app.use(cors())
 app.use(bodyParser.json())
-app.use(session(config.session))
+// app.use(session(config.session))
 
 masterRoutes(app);
 
 app.use("/", express.static(__dirname + '/public'))
-massive(config.postgres).then(dbInstance => {
+massive(process.env.DATABASE_URL).then(dbInstance => {
     app.set('db', dbInstance)
 })
 
 
-app.listen(port, function(){
+app.listen(process.env.PORT, function(){
     console.log('TTHHIIISS ISSS MYYY BOOOM STICK!!!')
 })
